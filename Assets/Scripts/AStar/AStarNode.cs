31ed2681733d5d0e2d3cf1be3bc84c01e64c88d6
Charles Cozart <charlescozart24@gmail.com>
2018-07-20 13:26:20 -0700

    AStar

---
+++ Assets/Scripts/AStar/AStarNode.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AStarNode : MonoBehaviour {
+
+    
+    [SerializeField]
+    private readonly GameObject data;
+    [SerializeField]
+    private List<AStarConnection> connections;
+
+    public AStarConnection ParentConnection { get; set; }
+
+
+    public void AddConnection(AStarConnection connection)
+    {
+        connections.Add(connection);
+    }
+
+    public void Removeconneciton(AStarConnection connection)
+    {
+        connections.Remove(connection);
+    }
+
+    public List<AStarConnection> GetConnections()
+    {
+        return connections;
+    }
+}
